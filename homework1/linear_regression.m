e006aa689449228b67eaefe4b4820e8e9b5709a4
xinzhiFan <xfanac@umich.edu>
2017-02-10 10:58:30 -0500

    hw1

--- homework1/linear_regression.m
+++ homework1/linear_regression.m
@@ -3,6 +3,19 @@ load xTrain.dat
 load yTrain.dat
 load xTest.dat
 load yTest.dat
-degree = 8;
+degree = 10;
+rms_train = zeros(1,10);
+rms_test = zeros(1,10);
+for degree = 0:9
 w=poly_regression( degree,xTrain,yTrain );
-y=regression_check( w,xTrain,yTrain );
+
+rms_train(degree+1)=regression_check( w,xTrain,yTrain );
+end
+for degree = 0:9
+w=poly_regression( degree,xTrain,yTrain );
+
+rms_test(degree+1)=regression_check( w,xTest,yTest );
+end
+hold on
+plot(rms_train,'b')
+plot(rms_test,'r')
--- homework1/regression_check.m
+++ homework1/regression_check.m
@@ -1,14 +1,14 @@
-function  y=regression_check( w,xTrain,yTrain )
+function  rms=regression_check( w,xTrain,yTrain )
 
 y=zeros(20,1);
+rms = 0;
 for j=1:20
 for i = 1:size(w,1)
     y(j) = w(i)*xTrain(j)^(i-1)+y(j);
 end
+rms = rms + (y(j) - yTrain(j))^2;
 end
-hold on
-plot(y,'r')
-plot(yTrain,'b')
 
+rms = sqrt(2*rms/20);
 end
 
